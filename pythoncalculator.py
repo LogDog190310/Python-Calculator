3443e4f80086fb346dea7b0602074db04e81743f
DDoS <116386707+LogDog190310@users.noreply.github.com>
2022-10-22 21:35:52 +0300

    Create pythoncalculator.py

---
+++ pythoncalculator.py
@@ -0,0 +1,15 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+c=(a+b)
+d=(a-b)
+e=(a*b)
+f=(a/b)
+
+print ("Sum:", c)
+print ("Difference:", d)
+print ("Product:", e)
+print ("Quotient:", f)
+
+print (input("To exit, press Enter:"))
+quit()
